f1df7082cf9a5198b5f09be7dc9d4d511f2f109e
misspriyapatidar <117170277+misspriyapatidar@users.noreply.github.com>
2023-04-21 15:54:44 +0530

    Add files via upload
    Add some changes

---
+++ about_us.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>About US</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    <style type="text/css">
+       body{
+       background-image: url("images/bg3.jpg");
+       background-size: 100%;
+       background-repeat: no-repeat;
+
+    }
+    
+  </style>
+</head>
+<body>
+   <h1 align="center" style="color:  Black">ABOUT US</h1> 
+
+  <!-- <p style="color: Black">Welcome to our Canteen Management System! Our software is designed to streamline the management of canteens, making it easier for administrators to keep track of orders, inventory, and finances.
+
+Our team is dedicated to providing a user-friendly and efficient solution that meets the needs of canteens of all sizes. With our system, canteen managers can easily create menus, track sales, manage inventory, and generate reports. Customers can also benefit from our software, as they can place orders online and receive notifications when their food is ready for pickup.
+
+We pride ourselves on delivering a high-quality product that can save canteen managers time and money. Our goal is to provide the best possible experience for both canteen administrators and their customers.</p>
+
+
+<br>
+
+
+<h1 align="center" style="color:  Black">CONTACT US</h1>
+
+  <p style="color: Black">A canteen management system is a software solution that is designed to streamline the management of canteens. This system can help canteen administrators to keep track of orders, inventory, and finances. It enables them to create menus, track sales, manage inventory, and generate reports with ease. The software can also benefit customers, as they can place orders online and receive notifications when their food is ready for pickup. The canteen management system can save time and money for canteen managers by automating many of the processes that were previously done manually. It can help to reduce errors, increase efficiency, and improve customer satisfaction. Overall, a canteen management system is an essential tool for any canteen that wants to improve its operations and provide better service to its customers.
+
+
+
+</p> --> 
+
+<!-- <h5 className="footer__title" style="color: Black">Contact</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <p style="color: Black">Location: St. Paul Institute of Professional Studies Indore, Lalaram Nagar, Indore, Madhya Pradesh</p>
+              </ListGroupItem>
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span style="color: Black">Phone: +91 731 2499911, 2490114</span>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span style="color: Black">Email: info@spipsindore.ac.in</span>
+              </ListGroupItem>
+            </ListGroup> -->
+
+
+  <div class="w3-row-padding w3-padding-16 w3-center" style="color:white">
+    <div class="w3-quarter">
+      <img class="img-rounded " src="images/kartik.png" alt="Kartik" style="width: 350px;height: 350px">
+      <h3>Vaibhav Parmar</h3>
+      <p> Backend Developer</p>
+    </div>
+    <div class="w3-quarter">
+      <img class="img-rounded " src="images/kalpu.png" alt="Kalpalatika" style="width: 350px;height: 350px">
+      <h3>Priya Patidar</h3>
+      <p>Front End Developer <p>  
+    </div>
+    <div><h5 className="footer__title" style="color: Black">Contact</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <p style="color: Black">Location: St. Paul Institute of Professional Studies Indore, Lalaram Nagar, Indore, Madhya Pradesh</p>
+              </ListGroupItem>
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span style="color: Black">Phone: +91 731 2499911, 2490114</span>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span style="color: Black">Email: info@spipsindore.ac.in</span>
+              </ListGroupItem>
+            </ListGroup> </div>
+    <!-- <div class="w3-quarter">
+      <img class="img-rounded " src="images/kp.png" alt="Kumaran" style="width: 350px;height: 350px">
+      <h3>Kumaran Paramesh</h3>
+      <p>Front End Developer</p>
+    </div>
+    <div class="w3-quarter">
+      <img class="img-rounded " src="images/ajit.png" alt="Ajit" style="width: 350px;height: 350px">
+      <h3>Ajit Kumar</h3>
+      <p>Front End Developer</p>
+    </div>
+
+
+<!-- <p style="color:white">&copy; 2017 SIES .GST&middot; CMS</p> -->
+  
+
+</body>
+</html>
+
+
